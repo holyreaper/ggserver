ac80ae7f795d81de4069551505fd712ae6f04b84
holyreaper <lihuaming45@126.com>
2017-11-14 18:10:52 +0800

    grpc

--- conf/conf.go
+++ conf/conf.go
@@ -1,1 +1,39 @@
 package conf
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+)
+
+//confMap := make(map[string]interface)
+//fileName 服务器配置
+var fileName = "service.json"
+
+//GetConf 获取服务器配置
+func GetConf() map[string]interface{} {
+	confMap := make(map[string]interface{})
+	conf, err := readFile()
+	if err == nil {
+		jserr := json.Unmarshal(conf, &confMap)
+		if jserr == nil {
+			return confMap
+		}
+		fmt.Printf("unMarshal data error  :%s", jserr.Error())
+	}
+	return nil
+
+}
+func init() {
+	fmt.Println("init conf")
+
+}
+
+func readFile() ([]byte, error) {
+	conf, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		fmt.Printf("cannot readfile%s error msg :%s", fileName, err.Error())
+		return nil, err
+	}
+	return conf, nil
+}
--- main.go
+++ main.go
@@ -1,12 +1,60 @@
 package main
 
 import (
+	//"context"
 	"fmt"
 
+	"github.com/holyreaper/ggserver/conf"
+
+	"runtime"
+
+	"net"
+
+	"github.com/holyreaper/ggserver/pb"
 	"github.com/holyreaper/ggserver/service"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+)
+
+const (
+	port = ":8090"
+	msg  = "helo lhm"
 )
 
 func main() {
-	fmt.Println("helo")
+	runtime.GOMAXPROCS(runtime.NumCPU())
 	service.Hello()
+	conf := conf.GetConf()
+	if data, ok := conf["info"]; ok {
+		sliceArray := data.([]interface{})
+		for k, v := range sliceArray {
+			fmt.Printf("key %d, value %s", k, v)
+		}
+	}
+
+	defer func() {
+		println("finish ...")
+	}()
+	listen, err := net.Listen("tcp", "127.0.0.1:8090")
+	if err != nil {
+		fmt.Println("listen fail ....")
+		panic(-1)
+	}
+
+	LoginServer := grpc.NewServer()
+
+	login.RegisterLoginServer(LoginServer, &LoginServerExt{})
+	LoginServer.Serve(listen)
+}
+
+//LoginServerExt struct
+type LoginServerExt struct {
+}
+
+/*
+SayHello ...
+helo world
+*/
+func (s *LoginServerExt) SayHello(context.Context, *login.HelloRequest) (*login.HelloReply, error) {
+	return &login.HelloReply{Message: msg}, nil
 }
